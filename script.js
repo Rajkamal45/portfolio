563e925d59dc0e622961ed77f3ffa52b763f33a0
Rajkamal kaushal <139054294+Rajkamal45@users.noreply.github.com>
2024-05-25 14:35:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Sample blog data (replace with your actual blog data)
 
 const blogPosts = [
-    { title: "First Blog Post", content: "chitra, jcnj nnjf ifnr rif r ", image: "path_to_image/image2.jpg" },
+    { title: "First Blog Post", content: "chitra, jcnj nnjf"<br>" ifnr rif r ", image: "path_to_image/image2.jpg" },
     { title: "Second Blog Post", content: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", image: "path_to_image/image2.jpg" },
     { title: "Third Blog Post", content: "Ut enim chahak adwani minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", image: "path_to_image/image3.jpg" },
     { title: "fourth Blog Post", content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", image: "path_to_image/image3.jpg" },
